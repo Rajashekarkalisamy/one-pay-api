182a677f1c332cb1b92336a81f5ce6d5c438031f
rajashekarkalisamy <rajashekarkalisamy@gmail.com>
2023-10-17 22:44:49 +0530

    cors headers added

--- app.js
+++ app.js
@@ -25,16 +25,25 @@ var app = express();
 /* view engine setup */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(logger('dev'));
+
+/* cors */
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+/* cors */
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(dbConfig.mongo.url, {
-  useNewUrlParser: true, 
+  useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
   console.log("Mongodb connected Successfully!");
